4777a372d63302bc55d13da6989b3e23e398b43e
Andrei <andrei@gmail.com>
2017-11-01 22:39:54 +0200

    started save system

---
+++ Assets/Editor/NodeUML/Scripts/Node.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[System.Serializable]
+public class Node
+{
+    public Rect Transform { private set; get; }
+
+    public string NodeName { private set; get; }
+
+    private int id;
+
+    [SerializeField]
+    private List<NodeInfoItem> listProperty;
+    [SerializeField]
+    private List<NodeInfoItem> listMethods;
+
+    public Node(Rect position, string name, int id)
+    {
+        NodeName = name;
+        Transform = position;
+        this.id = id;
+        listProperty = new List<NodeInfoItem>();
+        listMethods = new List<NodeInfoItem>();
+    }
+
+    public Node(string json)
+    {
+        JsonUtility.FromJsonOverwrite(json, this);
+    }
+
+    public override string ToString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+    public void DrawNode()
+    {
+        Transform = GUI.Window(id, Transform, UpdateNode, NodeName); 
+    }
+
+    private void UpdateNode(int id)
+    {
+        GUILayout.BeginVertical(GUILayout.Width(Transform.width - 15));
+        for (int i = 0; i < listProperty.Count; i++)
+        {
+            listProperty[i].Draw();
+        }
+
+        if (GUILayout.Button("New Property", GUILayout.Width(Transform.width - 15)))
+        {
+            listProperty.Add(new NodeInfoItem("", this));
+        }
+        GUILayout.Space(10);
+        for (int i = 0; i < listMethods.Count; i++)
+        {
+            listMethods[i].Draw();
+        }
+
+        if (GUILayout.Button("New Method", GUILayout.Width(Transform.width - 15)))
+        {
+            listMethods.Add(new NodeInfoItem("", this));
+        }
+        GUILayout.EndVertical();
+        GUI.DragWindow();
+    }
+
+    public void DeleteNodeInfo(NodeInfoItem info)
+    {
+        listProperty.Remove(info);
+        listMethods.Remove(info);
+    }
+}
+
+
---
+++ Assets/Editor/NodeUML/Scripts/NodeInfoItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NodeInfoItem
+{
+
+    public string Text{ private set; get; }
+
+    [System.NonSerialized]
+    private Node node;
+
+    public NodeInfoItem(string text, Node node)
+    {
+        Text = text;
+        this.node = node;
+    }
+
+    public NodeInfoItem(string json)
+    {
+        JsonUtility.FromJsonOverwrite(json, this);
+    }
+
+    public void Draw()
+    {
+        GUILayout.BeginHorizontal(GUILayout.Width(node.Transform.width - 15));
+        Text = GUILayout.TextField(Text, GUILayout.Width(node.Transform.width - 25));
+        if (GUILayout.Button("X", GUILayout.Width(15)))
+        {
+            node.DeleteNodeInfo(this);
+        }
+        GUILayout.EndHorizontal();
+    }
+
+    public override string ToString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+}
