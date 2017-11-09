03418fe9888fce7c784f5741b536996b29aefebe
Andrei Gusan <andrei.gusan@titanium-soft.com>
2017-11-09 17:32:41 +0200

    Imporved cuver relations, it changes the direction

--- Assets/Editor/NodeUML/Scripts/NodeRelation.cs
+++ Assets/Editor/NodeUML/Scripts/NodeRelation.cs
@@ -49,10 +49,22 @@ public class NodeRelation
 
     private void DrawNodeCurve(Rect start, Rect end)
     {
-        Vector3 startPos = new Vector3(start.x + start.width, start.y + start.height / 2, 0);
-        Vector3 endPos = new Vector3(end.x, end.y + end.height / 2, 0);
-        Vector3 startTan = startPos + Vector3.right * 50;
-        Vector3 endTan = endPos + Vector3.left * 50;
+        float direction = -1f;
+        float offcetStart = 0f;
+        float offcetEnd = 1f;
+
+        if (start.x < end.x)
+        {
+            direction = 1f;
+            offcetStart = 1f;
+            offcetEnd = 0f;
+        }
+
+        Vector3 startPos = new Vector3(start.x + start.width * offcetStart, start.y + start.height / 2, 0);
+        Vector3 endPos = new Vector3(end.x + end.width * offcetEnd, end.y + end.height / 2, 0);
+
+        Vector3 startTan = startPos + Vector3.right * 50 * direction;
+        Vector3 endTan = endPos + Vector3.left * 50 * direction;
         Handles.DrawBezier(startPos, endPos, startTan, endTan, Color.black, null, 2);
     }
 }
